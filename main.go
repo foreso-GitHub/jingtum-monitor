2fe5d12a0d609b26c35085fa8c35d1ad8112d8fe
foreso <foreso@sina.com>
2020-06-05 10:34:23 +0800

    Update jtLibrary. Add TPS flush function.

--- main.go
+++ main.go
@@ -2,24 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/foreso-GitHub/jingtum-monitor/common"
 	"github.com/foreso-GitHub/jingtum-monitor/exporter"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
 )
 
-//func init() {
-//	//注册自身采集器
-//	//prometheus.MustRegister(collector.NewNodeCollector())
-//	prometheus.MustRegister(exporter.NewJtCollector())
-//}
-
-//func main() {
-//	config := common.LoadConfig("./config/config.json")
-//	http.Handle("/metrics", promhttp.Handler())
-//	if err := http.ListenAndServe(config.ExportAddress, nil); err != nil {
-//		fmt.Printf("Error occur when start server %v", err)
-//	}
-//}
+func init() {
+	//注册自身采集器
+	//prometheus.MustRegister(collector.NewNodeCollector())
+	prometheus.MustRegister(exporter.NewJtCollector())
+}
 
 func main() {
+	test()
+
+	config := common.LoadConfig("./config/config.json")
+	http.Handle("/metrics", promhttp.Handler())
+	if err := http.ListenAndServe(config.ExportAddress, nil); err != nil {
+		fmt.Printf("Error occur when start server %v", err)
+	}
+}
+
+func test() {
 	testLibrary()
 	testTps()
 }
