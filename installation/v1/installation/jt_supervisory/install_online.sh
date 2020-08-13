dff8b37455677b83cabb7dfec8ba44607fca8587
foreso <foreso@sina.com>
2020-08-13 14:27:32 +0800

    Update jt_supervisory install scripts.

--- installation/v1/installation/jt_supervisory/install_online.sh
+++ installation/v1/installation/jt_supervisory/install_online.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 name_install="jt_supervisory-amd64-install-0.1.0"
 file_install=$name_install".tar.gz"
-url="https://www.yiyuen.com/e/file/download?code=afd649d70d70909b&id=28394"
+url="https://www.yiyuen.com/e/file/download?code=f33f826ece8555ac&id=28395"
 
 pwd
 output=`wget -O $file_install $url`
